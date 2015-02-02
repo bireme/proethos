d96bb2e516952ca9abd8e8aabde533e0cdff8ad6
Rene Faustino Gabriel Junior <renefgj@gmail.com>
2015-02-02 14:46:46 -0200

    #61

--- submit_end.php
+++ submit_end.php
@@ -51,7 +51,6 @@ if ($ok == 1)
 	/* Step 10 */
 	echo 'Enviando...';
 	$cep->confirm_submission_by_email();
-	exit;
 		
 	/* Step 2 */
 	$protocolo_cep = $cep->recupera_protocolo_submissao($protocolo,'1');
