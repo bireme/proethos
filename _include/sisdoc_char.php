9f44d268d763922de742ccfd83208243eec46e0a
Moacir Moda <moa.moda@gmail.com>
2015-09-03 16:42:40 -0300

    adicionando função de slugify, utilizada na criação do xml

--- _include/sisdoc_char.php
+++ _include/sisdoc_char.php
@@ -16,6 +16,30 @@ if (strlen($include) == 0) { exit; }
 //date_default_timezone_set('UTC'); 
 /**/
 
+function slugify($text) { 
+  // replace non letter or digits by -
+  $text = preg_replace('~[^\\pL\d]+~u', '-', $text);
+
+  // trim
+  $text = trim($text, '-');
+
+  // transliterate
+  $text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
+
+  // lowercase
+  $text = strtolower($text);
+
+  // remove unwanted characters
+  $text = preg_replace('~[^-\w]+~', '', $text);
+
+  if (empty($text))
+  {
+    return 'n-a';
+  }
+
+  return $text;
+}
+
 /* Detecta tipo de caracter */
 function codificacao($string) {
 	return mb_detect_encoding($string . 'x', 'UTF-8, ISO-8859-1');
