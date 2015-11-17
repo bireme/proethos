2611b62c1d9eb6f6a743f210327209ebddf81f57
Moacir Moda <moa.moda@gmail.com>
2015-11-17 11:06:43 -0200

    fix bug

--- register_unit_secundary.php
+++ register_unit_secundary.php
@@ -12,8 +12,9 @@
 // You should have received a copy of the ProEthos License along with the ProEthos
 // Software. If not, see
 // https://raw.githubusercontent.com/bireme/proethos/master/LICENSE.txt
+?>
 
-
+<?
  /**
   * Register Unit
   * @author Rene Faustino Gabriel Junior  (Analista-Desenvolvedor)
@@ -26,72 +27,12 @@
 $ref = 'register_'.$r.'_sec';
 $link = 'register_unit_secundary_ajax.php?dd1='.$protocolo.'&dd2=listar&dd3='.$autor.'&dd4='.$campo.'&dd6='.$ref.'&dd90='.checkpost($protocol.$campo);
 ?>
-<div id="<?
-// This file is part of the ProEthos Software. 
-// 
-// Copyright 2013, PAHO. All rights reserved. You can redistribute it and/or modify
-// ProEthos under the terms of the ProEthos License as published by PAHO, which
-// restricts commercial use of the Software. 
-// 
-// ProEthos is distributed in the hope that it will be useful, but WITHOUT ANY
-// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
-// PARTICULAR PURPOSE. See the ProEthos License for more details. 
-// 
-// You should have received a copy of the ProEthos License along with the ProEthos
-// Software. If not, see
-// https://raw.githubusercontent.com/bireme/proethos/master/LICENSE.txt
-
-=$ref;?>">
+<div id="<?=$ref;?>">
 </div>
 </fieldset>
 
 <script>
-	var $tela01 = $.ajax('<?
-// This file is part of the ProEthos Software. 
-// 
-// Copyright 2013, PAHO. All rights reserved. You can redistribute it and/or modify
-// ProEthos under the terms of the ProEthos License as published by PAHO, which
-// restricts commercial use of the Software. 
-// 
-// ProEthos is distributed in the hope that it will be useful, but WITHOUT ANY
-// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
-// PARTICULAR PURPOSE. See the ProEthos License for more details. 
-// 
-// You should have received a copy of the ProEthos License along with the ProEthos
-// Software. If not, see
-// https://raw.githubusercontent.com/bireme/proethos/master/LICENSE.txt
-
-=$link;?>')
-		.done(function(data) { $("#<?
-// This file is part of the ProEthos Software. 
-// 
-// Copyright 2013, PAHO. All rights reserved. You can redistribute it and/or modify
-// ProEthos under the terms of the ProEthos License as published by PAHO, which
-// restricts commercial use of the Software. 
-// 
-// ProEthos is distributed in the hope that it will be useful, but WITHOUT ANY
-// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
-// PARTICULAR PURPOSE. See the ProEthos License for more details. 
-// 
-// You should have received a copy of the ProEthos License along with the ProEthos
-// Software. If not, see
-// https://raw.githubusercontent.com/bireme/proethos/master/LICENSE.txt
-
-=$ref;?>").html(data); })
-		.always(function(data) { $("#<?
-// This file is part of the ProEthos Software. 
-// 
-// Copyright 2013, PAHO. All rights reserved. You can redistribute it and/or modify
-// ProEthos under the terms of the ProEthos License as published by PAHO, which
-// restricts commercial use of the Software. 
-// 
-// ProEthos is distributed in the hope that it will be useful, but WITHOUT ANY
-// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
-// PARTICULAR PURPOSE. See the ProEthos License for more details. 
-// 
-// You should have received a copy of the ProEthos License along with the ProEthos
-// Software. If not, see
-// https://raw.githubusercontent.com/bireme/proethos/master/LICENSE.txt
-
-=$ref;?>").html(data); });	 
+  var $tela01 = $.ajax('<?=$link;?>')
+    .done(function(data) { $("#<?=$ref;?>").html(data); })
+    .always(function(data) { $("#<?=$ref;?>").html(data); });  
 </script>
